06901597f5e6d494f36e0c3bcc4b7562aa12bf4b
hanhpham327 <hanhtp2001@gmail.com>
2018-04-04 10:36:06 -0700

    Hanh has generated the gameboard with jquery and css with mainJs file and css file

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(initializeApp);
+function initializeApp(){
+    buildGameBoard();
+}
+
+function buildGameBoard(){
+    var boardSize = { rows: 8, squares: 8 };
+    var gameBoard = $('#game-board');
+    for (var newRow=0;newRow<boardSize.rows;newRow++){
+        var divsToAppend;
+        divsToAppend=$("<div>").addClass("row")    
+        for(var newSquare=0;newSquare<boardSize.rows;newSquare++){       
+            var showSquare=$("<div>").addClass("square")    
+            if(newSquare%2===0 && newRow%2===0){
+                showSquare.addClass("light")    
+            } else if(newSquare%2===1 && newRow%2===1){
+                showSquare.addClass("light")    
+            } else if(newSquare%2===1 && newRow%2===0){
+                showSquare.addClass("dark")    
+            } else{
+                showSquare.addClass("dark")    
+            }
+            // showSquare.appendTo(divsToAppend)
+            divsToAppend.append(showSquare)
+        }
+        $("#game-board").append(divsToAppend)
+    }
+
+}
